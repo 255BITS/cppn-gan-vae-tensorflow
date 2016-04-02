0663ff633b8deaebc6fecf974afb1b23ce821991
Mikkel Garcia <mikkel@255bits.com>
2016-04-01 22:51:12 -0600

    [fix] create samples works

--- create_samples.py
+++ create_samples.py
@@ -1,9 +1,20 @@
 from sampler import *
+import os
+from glob import glob
+import tensorflow_wav
 
 sampler = Sampler()
+
+data = glob(os.path.join("./training", "*.wav"))
+sample_file = data[0]
+sample =tensorflow_wav.get_wav(sample_file)
 
 z = sampler.generate_z()
 for i in range(100):
     scale = i+1
     imagedata = sampler.generate(z, x_dim=64, y_dim=64, scale=scale)
-    sampler.save_png(imagedata, "samples/sample-"+str(scale)+".wav")
+    samplewav = sample.copy()
+    samplewav['data']=np.dot(imagedata, 65535)
+
+    filename = "samples/song-"+str(i)+".wav"
+    tensorflow_wav.save_wav(samplewav, filename )
